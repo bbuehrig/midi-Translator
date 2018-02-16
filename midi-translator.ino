f916e7a000355a4c05ecf1544df6b6b2dbaa33c5
Bastian Bührig <mail@bastian-buehrig.de>
2018-02-16 16:40:07 +0100

    If-Else removed. Switch-Case added.

--- midi-translator.ino
+++ midi-translator.ino
@@ -20,8 +20,8 @@ MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, midi::DefaultSettings)
 
 
 /**
- * Setup your Arduino!
- */
+   Setup your Arduino!
+*/
 void setup() {
   // MIDI listening to device channel
   MIDI.begin(CHANNEL_MIDI_DEVICE);
@@ -39,21 +39,21 @@ void setup() {
 
 
 /**
- * Start reading MIDI-Inputs...
- */
+   Start reading MIDI-Inputs...
+*/
 void loop() {
   MIDI.read();
 }
 
 
 /**
- * Handle all incoming ControlChange messages. We want to echo all incoming
- * ControlChange message only. This is for expression pedals! 
- * 
- * @param channel MIDI channel for incoming MIDI ControlChange message
- * @param number  number of controller that was received
- * @param value   value of invcoming ControlChange message. Possible values: 0-127
- */
+   Handle all incoming ControlChange messages. We want to echo all incoming
+   ControlChange message only. This is for expression pedals!
+
+   @param channel MIDI channel for incoming MIDI ControlChange message
+   @param number  number of controller that was received
+   @param value   value of invcoming ControlChange message. Possible values: 0-127
+*/
 void handlerControlChange(byte channel, byte number, byte value) {
   // Log all incoming ControlChange messages...
   // char logstring[70];
@@ -65,15 +65,15 @@ void handlerControlChange(byte channel, byte number, byte value) {
 
 
 /**
- * Handle all incoming ProgramChange messages. There is a translation implemented. For
- * all different ProgramChange numbers it is possibility to send any MIDI-message out
- * to your MIDI-device. Here we react to ProgramChange 0 - 4. It will be translated to
- * ControlChange-message on controler number 34. It sends value 0 - 4. ControlChange 34
- * is the default ControlChange number for scene-switching in Fractal Audio Axe-FX II.
- * 
- * @param channel MIDI channel for incoming MIDI ProgramChange message. 
- * @param number  Incoming program-number.
- */
+   Handle all incoming ProgramChange messages. There is a translation implemented. For
+   all different ProgramChange numbers it is possibility to send any MIDI-message out
+   to your MIDI-device. Here we react to ProgramChange 0 - 4. It will be translated to
+   ControlChange-message on controler number 34. It sends value 0 - 4. ControlChange 34
+   is the default ControlChange number for scene-switching in Fractal Audio Axe-FX II.
+
+   @param channel MIDI channel for incoming MIDI ProgramChange message.
+   @param number  Incoming program-number.
+*/
 void handlerProgramChange(byte channel, byte number) {
   // Log all incoming ProgramChange messages...
   // char logstring[70];
@@ -81,34 +81,41 @@ void handlerProgramChange(byte channel, byte number) {
   // Serial.println(logstring);
 
   // Do the translation-stuff...
-  if (number == 0) {
-    // Switch to scene 1
-    // Serial.println("Switch to Axe-FX Scene 1");
-    MIDI.sendControlChange(34, 0, CHANNEL_MIDI_DEVICE);
-    
-  } else if (number == 1) {
-    // Switch to scene 2
-    // Serial.println("Switch to Axe-FX Scene 2");
-    MIDI.sendControlChange(34, 1, CHANNEL_MIDI_DEVICE);
-    
-  }  else if (number == 2) {
-    // Switch to scene 3
-    // Serial.println("Switch to Axe-FX Scene 3");
-    MIDI.sendControlChange(34, 2, CHANNEL_MIDI_DEVICE);
-    
-  }  else if (number == 3) {
-    // Switch to scene 4
-    // Serial.println("Switch to Axe-FX Scene 4");
-    MIDI.sendControlChange(34, 3, CHANNEL_MIDI_DEVICE);
-    
-  }  else if (number == 4) {
-    // Switch to scene 5
-    // Serial.println("Switch to Axe-FX Scene 5");
-    MIDI.sendControlChange(34, 4, CHANNEL_MIDI_DEVICE);
-    
-  } else {
-    // All other ProgramChange messages will be echoed to MIDI-Out. 
-    MIDI.sendProgramChange(number, CHANNEL_MIDI_DEVICE);
+  switch (number)  {
+    case 0:
+      // Switch to scene 1
+      // Serial.println("Switch to Axe-FX Scene 1");
+      MIDI.sendControlChange(34, 0, CHANNEL_MIDI_DEVICE);
+      break;
+
+    case 1:
+      // Switch to scene 2
+      // Serial.println("Switch to Axe-FX Scene 2");
+      MIDI.sendControlChange(34, 1, CHANNEL_MIDI_DEVICE);
+      break;
+
+    case 2:
+      // Switch to scene 3
+      // Serial.println("Switch to Axe-FX Scene 3");
+      MIDI.sendControlChange(34, 2, CHANNEL_MIDI_DEVICE);
+      break;
+
+    case 3:
+      // Switch to scene 4
+      // Serial.println("Switch to Axe-FX Scene 4");
+      MIDI.sendControlChange(34, 3, CHANNEL_MIDI_DEVICE);
+      break;
+
+    case 4:
+      // Switch to scene 5
+      // Serial.println("Switch to Axe-FX Scene 5");
+      MIDI.sendControlChange(34, 4, CHANNEL_MIDI_DEVICE);
+      break;
+
+    default:
+      // All other ProgramChange messages will be echoed to MIDI-Out.
+      MIDI.sendProgramChange(number, CHANNEL_MIDI_DEVICE);
+      break;
   }
 }
 
